da822e7d464e2582e32fbc01efbaa1c99234f2fb
postscript91 <piotr@slomczykowski.pl>
2025-03-18 19:26:00 +0100

    creating player object and it's physics

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import *
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = (updatable, drawable)
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
