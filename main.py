a0d7b550491cde59c439c791e6d027baf81e413d
postscript91 <piotr@slomczykowski.pl>
2025-03-19 17:47:46 +0100

    finishing the project by adding asteroids, shooting and splitting

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import pygame
+import sys
 from constants import *
+from circleshape import *
 from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import *
+
 updatable = pygame.sprite.Group()
 drawable = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
+shots = pygame.sprite.Group()
 Player.containers = (updatable, drawable)
+Asteroid.containers = (asteroids, updatable, drawable)
+AsteroidField.containers = updatable
+Shot.containers = (shots, updatable, drawable)
+asteroid_field = AsteroidField()
 
 def main():
     pygame.init()
@@ -19,6 +31,14 @@ def main():
         updatable.update(dt)
         for d in drawable:
             d.draw(screen)
+        for asteroid in asteroids:
+            if asteroid.collision_check(player):
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                if asteroid.collision_check(shot):
+                    asteroid.split()
+                    shot.kill()
         pygame.display.flip()
         dt = clock.tick(60) / 1000
     
